9163d6b877daa6e2895f5956b0f457a1200cc423
Ubuntu <ubuntu@ip-172-31-27-228.ec2.internal>
2024-10-09 05:49:47 +0000

    Refactored getSentiment.py and newsGetter.py

--- SentimentAnalysis/newsGetter.py
+++ SentimentAnalysis/newsGetter.py
@@ -23,31 +23,38 @@ import xml.etree.ElementTree as ET
 
 #     for child in children: # Iterate through the XML and print elements
 #         print(f"Tag: {child.tag}, Attributes: {child.attrib}, Text: {child.text}")
+class NewsGetter:
+    def __init__(self, link):
+        self.url = link
+    def get_top_headlines():
+        response = requests.get(url)
+        if response.status_code == 200:
+            root = ET.fromstring(response.content)
+            headlines = []
+            for item in root.findall('.//item'):
+                title = item.find('title').text
+                headlines.append(title)
+            return headlines
+        else:
+            return []
+    def get_headlines_with_descriptions(url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            root = ET.fromstring(response.content)
+            headlines_with_descriptions = []
+            for item in root.findall('.//item'):
+                title = item.find('title').text
+                description = item.find('description').text
+                headlines_with_descriptions.append({'title': title, 'description': description})
+            return headlines_with_descriptions
+        else:
+            return
 
-def get_top_headlines(url):
-            response = requests.get(url)
-            if response.status_code == 200:
-                root = ET.fromstring(response.content)
-                headlines = []
-                for item in root.findall('.//item'):
-                    title = item.find('title').text
-                    headlines.append(title)
-                return headlines
-            else:
-                return []
-def get_headlines_with_descriptions(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        root = ET.fromstring(response.content)
-        headlines_with_descriptions = []
-        for item in root.findall('.//item'):
-            title = item.find('title').text
-            description = item.find('description').text
-            headlines_with_descriptions.append({'title': title, 'description': description})
-        return headlines_with_descriptions
-    else:
-        return []
 # Example usage
-url = 'https://feeds.a.dj.com/rss/RSSMarketsMain.xml'
-top_headlines = get_top_headlines(url)
-print(top_headlines)
+if __name__ == '__main__':
+    url = 'https://feeds.a.dj.com/rss/RSSMarketsMain.xml'
+    news_getter = NewsGetter(url)
+    top_headlines = NewsGetter.get_top_headlines()
+    top_headlines_with_descriptions = NewsGetter.get_headlines_with_descriptions(url)
+    print(top_headlines)
+    print(top_headlines_with_descriptions)
