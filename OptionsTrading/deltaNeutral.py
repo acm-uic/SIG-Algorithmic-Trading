0558dabd40e981c4f3c092ed60f46e18bd5f8966
Connor Hunter <59147712+Mohpann@users.noreply.github.com>
2024-10-11 21:00:46 -0500

    Update deltaNeutral.py

--- OptionsTrading/deltaNeutral.py
+++ OptionsTrading/deltaNeutral.py
@@ -11,5 +11,5 @@ Source: https://www.cmegroup.com/education/courses/option-greeks/options-delta-t
 TO DO: Caluclate the delta value for DJT (Trump Media: Stock) in any way you see fit. Be sure 
 to make it clear what time horizon your delta is based off of.
 
-The formula of delta = Change in the Price of Asset / Change in the Price of Underlying.
+The formula of delta = Change in the Price of Option / Change in the Price of Underlying.
 '''
