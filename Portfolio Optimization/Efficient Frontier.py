434ecb6c01f61af727e8f325412d341bbba3cb5e
Tuong Max Le <150745707+TUTULEMAN@users.noreply.github.com>
2024-10-16 01:39:31 -0500

    Update Efficient Frontier.py

--- Portfolio Optimization/Efficient Frontier.py
+++ Portfolio Optimization/Efficient Frontier.py
@@ -1,6 +1,24 @@
-#pip install quantpy
+'''
+Explanation of Efficient Frontier:
+    - The efficient frontier, also known as the portfolio frontier, is a set of ideal or optimal portfolios that are expected to give 
+    the highest return for a minimal level of return12. It is formed by plotting the expected return on the y-axis and the standard 
+    deviation as a measure of risk on the x-axis1. 
+    - Portfolios that lie below the efficient frontier are sub-optimal because they do not provide enough return for the level of risk.
+    - Great video to visualize this--> https://www.youtube.com/watch?v=AGjsvdDMyhE&t=1s
 
+Explanation of code:
+    1. Plots Efficient Frontier (to visualize portfolio asset distribution)
+    2. Calculates optiomal portfolio weights
+    3. Comuptes expected retuns + volatility + sharpe ratio (=expected returns/volatility)
+    4. Prints allocation of different asset classes (using data from 3)
+    note: allows users to type in their preferred stocks using get-user_ticker() func
 
+Pre-requisite:
+    -------------------------
+    |  pip install quantpy |
+    -------------------------
+'''
+#---------------------------
 
 import quantpy as qp
 import numpy as np
